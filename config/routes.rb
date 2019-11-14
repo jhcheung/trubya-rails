822e1f692cfeb96c887e3ec13025756f33ecdd00
Jeffrey Paulino <jp7kode@gmail.com>
2019-11-13 21:25:40 -0500

    Done with user create and logins page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy' 
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
   get '/home', to: 'welcome#home'
 
 
